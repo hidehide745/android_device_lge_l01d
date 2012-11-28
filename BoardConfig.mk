e5ff16db10da202bf92c2c606704160c61e8df42
nekomimipanic <nekomimip@gmail.com>
2012-11-29 05:22:14 +0900

    modified for L-01D(i_dcm)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,12 @@
 # inherit from the proprietary version
--include vendor/lge/p930/BoardConfigVendor.mk
+-include vendor/lge/i_dcm/BoardConfigVendor.mk
 
 -include device/lge/iprj-common/BoardConfigCommon.mk
 
-TARGET_BOOTLOADER_BOARD_NAME := p930
-TARGET_OTA_ASSERT_DEVICE := p930
+TARGET_BOOTLOADER_BOARD_NAME := i_dcm
+TARGET_OTA_ASSERT_DEVICE := i_dcm
 
 # Try to build the kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_p930_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_l01d_defconfig
 # Keep this as a fallback
-TARGET_PREBUILT_KERNEL := device/lge/p930/kernel
+TARGET_PREBUILT_KERNEL := device/lge/i_dcm/kernel
